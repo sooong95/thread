e04c6210c8877d2bd256248390a8dbb1312c2ec2
song <sooong137@gmail.com>
2025-02-06 17:56:49 +0900

    thread 02-06

---
+++ src/thread/control/CheckedExceptionMain.java
@@ -0,0 +1,20 @@
+package thread.control;
+
+import util.ThreadUtils;
+
+import static util.ThreadUtils.sleep;
+
+public class CheckedExceptionMain {
+
+    public static void main(String[] args) throws Exception {
+        throw new Exception();
+    }
+
+    static class CheckedRunnable implements Runnable {
+
+        @Override
+        public void run() {
+            sleep(1000);
+        }
+    }
+}
