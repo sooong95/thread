6e4d74fff5cfc1ef68e05da97ecb1640f3fd1b09
song <sooong137@gmail.com>
2025-02-11 16:52:12 +0900

    reentrantLock, producer, consumer 02-11

--- src/thread/sync/BankMain.java
+++ src/thread/sync/BankMain.java
@@ -8,7 +8,7 @@ import static util.ThreadUtils.sleep;
 public class BankMain {
 
     public static void main(String[] args) throws InterruptedException {
-        BankAccountV3 account = new BankAccountV3(1000);
+        BankAccountV6 account = new BankAccountV6(1000);
 
         Thread t1 = new Thread(new WithdrawTask(account, 800), "t1");
         Thread t2 = new Thread(new WithdrawTask(account, 800), "t2");
